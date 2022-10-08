eb7834b018a56d84ba2e07468f0787bfb45887df
KonstantinSuvorov <106460617+KonstantinSuvorov@users.noreply.github.com>
2022-10-08 14:44:23 +0300

    Update TrafficLight.py

--- TrafficLight.py
+++ TrafficLight.py
@@ -73,34 +73,3 @@ tl3.running()
 tl1 = TrafficLight('красный')
 tl1.running('желтый')
 
-
-
-
-# ВАРИАНТ 2
-# from time import sleep
-# from datetime import datetime as dt
-
-
-# class TrafficLight:
-#     """ Класс светофора, реализующий свое переключение при запуске running( """
-#     _states = {'red': 7, 'yellow': 2, 'green': 10}
-#     _color = ''
-
-#     def running(self):
-#         """ Метод запуск светофора """
-#         for color, sw_time in self._states.items():
-#             self._color = color
-#             start_state_time = dt.now()
-
-#             print(f"TrafficLight switched to state '{self._color}' "
-#                   f"on {sw_time} seconds")
-
-#             sleep(sw_time)
-
-#             print(f"TrafficLight leave state '{self._color}' after " 
-#                   f"{(dt.now() - start_state_time).seconds} seconds")
-
-
-# if __name__ == '__main__':
-#     tl = TrafficLight()
-#     tl.running()
